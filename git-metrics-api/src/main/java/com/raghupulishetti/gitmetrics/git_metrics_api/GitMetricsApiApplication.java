a21b455439f726189beb06baf78de3cab47d37d4
Raghupathi Pulishetti <raghupulishetti@gmail.com>
2024-12-13 14:19:45 -0600

    Initial commit.

---
+++ git-metrics-api/src/main/java/com/raghupulishetti/gitmetrics/git_metrics_api/GitMetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.raghupulishetti.gitmetrics.git_metrics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitMetricsApiApplication.class, args);
+	}
+
+}
---
+++ git-metrics-api/src/test/java/com/raghupulishetti/gitmetrics/git_metrics_api/GitMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghupulishetti.gitmetrics.git_metrics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
